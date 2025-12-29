a75dd4d08c41882dd8a420123adbd6303fb2d129
Hassan Bahaa <66232742+hassanbahaa@users.noreply.github.com>
2025-12-29 23:23:38 +0200

    fix

--- index.js
+++ index.js
@@ -13,16 +13,3 @@ readStream.on("data", (chunk) => {
   console.log("data chunk received: ", chunk);
   console.log("========================");
 });
-
-// 2 - Use readable and writable streams to copy content from one file to another.
-const readAndWriteStream = fs.createReadStream("./big.txt");
-readStream.on("open", () => {
-  console.log("file open");
-});
-readStream.on("ready", () => {
-  console.log("file is ready to be read");
-});
-readStream.on("data", (chunk) => {
-  console.log("data chunk received: ", chunk);
-  console.log("========================");
-});
